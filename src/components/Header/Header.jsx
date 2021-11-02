ed5d332640aa1000e70f80e7b82465cae6359649
Emad Soltan <emsoltan2020@gmail.com>
2021-11-02 15:50:00 +0100

    icanhelp.de react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,9 +11,12 @@ const Header = () => {
           <span className="heading-primary-main">Icanhelp.DE</span>
           <span className="heading-primary-sub">We can do better together</span>
         </h1>
-        <button className="btn btn-white btn-animated">
+        <a
+          href="https://icanhelp.vercel.app/"
+          className="btn btn-white btn-animated"
+        >
           check good deeds
-        </button>
+        </a>
       </div>
     </header>
   );
